7c463a314a8ef9a877af148aabb5df09b8ac6d12
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2015-10-25 23:04:50 +0000

    Modify custom title to always display post page url.

--- package.js
+++ package.js
@@ -32,7 +32,6 @@ Package.onUse(function (api) {
     'lib/client/templates/hello.html',
     'lib/client/templates/hello.js',
     'lib/client/templates/custom_post_title.html',
-    'lib/client/templates/custom_post_title.js',
     'lib/client/stylesheets/custom.scss',
     'lib/client/custom_templates.js'
   ], ['client']);
