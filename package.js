74d660c695dedf18ef8fe6817dd8a75518ffaaef
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2017-03-26 17:20:38 +0100

    telescope-share: Remove overriding class since package is removed.

--- package.js
+++ package.js
@@ -42,7 +42,6 @@ Package.onUse(function (api) {
     'lib/client/templates/custom_post_body.js',
     'lib/client/templates/custom_post_edit.js',
     'lib/client/templates/custom_post_submit.js',
-    'lib/client/templates/custom_post_share.js',
     'lib/client/templates/custom_views_menu.html',
     'lib/client/templates/custom_submit_button.html',
     'lib/client/templates/custom_footer_code.html',
